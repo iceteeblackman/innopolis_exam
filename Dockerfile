1b0cfe00d674648e7723a93564abc15f93e22080
Kriukov Pavel <iceteeblackman@mail.ru>
2020-12-10 19:33:38 +0700

    added docker file

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.8.6-buster
+MAINTAINER Kriukov Pavel
+RUN apt-get update -y
+RUN apt-get install -y gunicorn
+RUN pip install --upgrade setuptools
+COPY . /app
+WORKDIR /app
+RUN pip install -r requirements.txt
+EXPOSE 8003
+CMD gunicorn -w 5 --reload start:app --bind 0.0.0.0:8003
--- exam/__init__.py
+++ exam/__init__.py
@@ -1,21 +1,21 @@
-import os
+import os.path
 
 from flask import Flask
 from flask_autoindex import AutoIndex
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
-UPLOAD_FOLDER = '/home/itbm/dev/innopolis_exam/'
+UPLOAD_FOLDER = os.environ['USER_FOLDER']
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.secret_key = 'some secret salt'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://itbm:_1q2w3e4r@localhost/innopolis_app'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://itbm:_1q2w3e4r@172.17.0.2/innopolis_app'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
 manager = LoginManager(app)
-files_index = AutoIndex(app, os.path.curdir + '/dev/innopolis_exam', add_url_rules=False)
+files_index = AutoIndex(app, os.environ['USER_FOLDER'], add_url_rules=False)
 
 from exam import models, routes
 
